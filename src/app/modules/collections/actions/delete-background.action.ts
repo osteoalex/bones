89eb75f334773c4e310636003d071f280b4cf1e6
Maciej Krawczyk <maciejro.krawczyk@gmail.com>
2025-10-21 22:47:10 +0200

    build fix

--- src/app/modules/collections/actions/delete-background.action.ts
+++ src/app/modules/collections/actions/delete-background.action.ts
@@ -1,7 +1,7 @@
 import { TAction } from '../../../../types/store.types';
 import { setLoading } from '../slices/ui.slice';
 
-export function deleteBackgroundAction(bg: string): TAction<Promise<boolean>> {
+export function deleteBackgroundAction(bg: string): TAction<Promise<void>> {
   return async (dispatch) => {
     dispatch(setLoading(true));
     await window.electron.deleteBackground(bg);
